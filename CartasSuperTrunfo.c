75edd7995b03b9f0453e0f148449ed51169f0ede
renanbandeira148 <renanbandeira148@gmail.com>
2025-10-22 00:31:55 +0000

    Fiz entrada de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,15 +8,39 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   
   char estado;
-  char codigo_carta[5];
-  char nome_cidade[20];
+  char codigo_carta [5];
+  char nome_cidade [20];
   int populacao;
   float area;
   float pib;
-  int quant_turismo;
-  
+  int pontos_turismo;
+
   // Área para entrada de dados
 
+  printf("Super Trunfo C \n");
+  printf ("Carta 1 \n");
+
+  printf ("Digite o estado: \n");
+  scanf ("%c", &estado);
+
+  printf("Digite o Codigo da Carta: \n");
+  scanf("%s", codigo_carta);
+
+  printf ("Digite o nome da cidade: \n");
+  scanf("%s", nome_cidade);
+
+  printf ("Digite a populacao: \n");
+  scanf ("%d", populacao);
+
+  printf("Digite a area em Km: \n");
+  scanf("%f", area);
+
+  printf("Digite o PIB: \n");
+  scanf("%f", pib);
+
+  printf("Digite a quantidade de pontos turisticos: \n");
+  scanf ("%d", pontos_turismo);
+  
   // Área para exibição dos dados da cidade
 
 return 0;
